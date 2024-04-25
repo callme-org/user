8ec1c45dd471bb8ba200f5ad58be0509667252f4
comougi <comougi@gmail.com>
2024-04-25 19:23:21 +0300

    handle already inserted exception

--- src/main/kotlin/com/ougi/callme/presentation/routing/CreateUser.kt
+++ src/main/kotlin/com/ougi/callme/presentation/routing/CreateUser.kt
@@ -7,18 +7,33 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.koin.ktor.ext.inject
+import java.sql.SQLIntegrityConstraintViolationException
 
 fun Route.create() {
     val createUserUseCase by inject<CreateUserUseCase>()
     post("/create") {
         val userLogin = call.receiveNullable<UserLogin>()
         userLogin?.login?.let { login ->
-            createUserUseCase.createUser(login)
-            call.respond(
-                HttpStatusCode.OK,
-                "User successfully created"
-            )
-        } ?: call.respond(HttpStatusCode.Conflict)
+            runCatching {
+                createUserUseCase.createUser(login)
+                call.respond(
+                    status = HttpStatusCode.OK,
+                    message = "User successfully created"
+                )
+            }.onFailure { th ->
+                if (th is ExposedSQLException && th.cause is SQLIntegrityConstraintViolationException)
+                    call.respond(
+                        status = HttpStatusCode.Conflict,
+                        message = "User already created"
+                    )
+                else
+                    call.respond(
+                        status = HttpStatusCode.BadRequest,
+                        message = th.message ?: th.localizedMessage
+                    )
+            }
+        } ?: call.respond(HttpStatusCode.BadRequest)
     }
 }
